99b4fb7dfbb27e779fbd79ea1bebb8f2f79f7aec
Samet Özenç <66252675+SametOzenc@users.noreply.github.com>
2022-04-20 17:24:31 +0300

    Update Updated_Mesh&Water_Pressures.py

--- Toolbox/Updated_Mesh&Water_Pressures.py
+++ Toolbox/Updated_Mesh&Water_Pressures.py
@@ -1,11 +1,10 @@
 from plxscripting.easy import *
-s_i, g_i = new_server('localhost', 10000, password="ek1g7@W4@Gev?@Q?")
+s_i, g_i = new_server()
 
 def main():
     for phase in g_i.Phases[1:]:
         if not phase.DeformCalcType == phase.DeformCalcType.fullycoupledflowdeformation:
             phase.Deform.UseUpdatedMesh = True
-            if not phase.DeformCalcType == phase.DeformCalcType.dynamic or not phase.DeformCalcType == phase.DeformCalcType.dynamicwithconsolidation:
+            if phase.DeformCalcType != phase.DeformCalcType.dynamic and not phase.DeformCalcType == phase.DeformCalcType.dynamicwithconsolidation:
                 phase.Deform.UseUpdatedWaterPressures = True
-
 main()
