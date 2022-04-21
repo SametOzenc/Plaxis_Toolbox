6c9031b7dc3fdaa3d5c381a4b4c3ba607dddeeee
SametOzenc <sametozenc@gmail.com>
2022-04-21 17:27:24 +0300

    Added some comments

--- Toolbox/Mesh&Run.py
+++ Toolbox/Mesh&Run.py
@@ -1,7 +1,18 @@
+# BOILERPLATE for PLAXIS #
+# To be used when running via Expert > Run python tool
 from plxscripting.easy import *
-s_i, g_i = new_server()
+s_i, g_i = new_server()  
+
+# To be used when running via SciTE
+# localhostport_i = 10000
+# password = 'YOUR_PASSWORD'
+# s_i, g_i = new_server('localhost', localhostport_i, password=password)
+# END OF BOILERPLATE ##
 
 def main():
+    """
+    Mesh medium element distribution and calculate
+    """
     g_i.gotomesh()
     g_i.mesh()
     g_i.gotostages()
--- Toolbox/Updated_Mesh&Water_Pressures.py
+++ Toolbox/Updated_Mesh&Water_Pressures.py
@@ -1,10 +1,21 @@
+# BOILERPLATE for PLAXIS #
+# To be used when running via Expert > Run python tool
 from plxscripting.easy import *
-s_i, g_i = new_server()
+s_i, g_i = new_server()  
+
+# To be used when running via SciTE
+# localhostport_i = 10000
+# password = 'YOUR_PASSWORD'
+# s_i, g_i = new_server('localhost', localhostport_i, password=password)
+# END OF BOILERPLATE ##
 
 def main():
+    """
+    Activates the Updated Mesh and Updated Water Pressures option in all phases except the Initial phase
+    """
     for phase in g_i.Phases[1:]:
-        if not phase.DeformCalcType == phase.DeformCalcType.fullycoupledflowdeformation:
-            phase.Deform.UseUpdatedMesh = True
-            if phase.DeformCalcType != phase.DeformCalcType.dynamic and not phase.DeformCalcType == phase.DeformCalcType.dynamicwithconsolidation:
-                phase.Deform.UseUpdatedWaterPressures = True
+        if not phase.DeformCalcType == phase.DeformCalcType.fullycoupledflowdeformation: #There is no Uptaded Mesh option for Fully coupled flow-deformatio calculation type  
+            phase.Deform.UseUpdatedMesh = True #Activates the Updated Mesh option
+            if phase.DeformCalcType != phase.DeformCalcType.dynamic and not phase.DeformCalcType == phase.DeformCalcType.dynamicwithconsolidation: #There is no Updated Water Pressures option for dynamic and dynamic with consolidation calculation type  
+                phase.Deform.UseUpdatedWaterPressures = True #Activates the Updated Mesh and Updated Water Pressures option
 main()
